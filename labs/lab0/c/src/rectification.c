48358f98754fa264f99ce922950954e0a53877b7
Cytosine <pengnan.fan@mail.mcgill.ca>
2020-09-30 01:03:36 +0800

    123

--- labs/lab0/c/src/rectification.c
+++ labs/lab0/c/src/rectification.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 #include "lodepng.h"
+
+int check_if_png(char *name);
 
 int main(int argc, char *argv[]){
 	
@@ -14,26 +17,37 @@ int main(int argc, char *argv[]){
 
 	printf("There are %d PNG files to be processed:\n", argc-1);
 	
-	char **png_path = (char **)calloc(argc-1, sizeof(char *));
-	
 	// Read the arguments suggesting the path to PNGs
 	for (int i = 0; i<argc-1; i++) {
 		//printf("%d: %s\n", count, argv[count]);
-		int len = strlen(argv[i+1])+1;
-		*png_path = (char *)calloc(len, sizeof(char));
-		memcpy(*png_path, argv[i+1], len);
-		printf("%d: %s\n", i, *png_path);
-		*png_path+=1;
-	}
+		printf("%d: %s\n", i, argv[i+1]);
+		
+		if (check_if_png(argv[i+1])) {
+			//TODO: add read file
 
-	//wipe out
-	for (int i = 0; i<argc-1; i++) {
-		*png_path = NULL;
-		free(*png_path);
-		*png_path-=1;
-	}
+			//TODO: add rectification
 
-	free(png_path);
+			//TODO: add save file
+		} else {
+			printf("Invalid PNG file: %s\n", argv[i+1]);
+		}
+	}
 
 	return 0;
+}
+
+int check_if_png(char *name){
+	regex_t reg;
+	const char *pattern = "^.*\\.(png)?$";
+	regcomp(&reg, pattern, REG_EXTENDED);
+	const size_t nmatch = 1;
+	regmatch_t pmatch[1];
+	int status = regexec(&reg, name, nmatch, pmatch, 0);
+	regfree(&reg);
+
+	if (status == REG_NOMATCH){
+		return 0;
+	} else if (status == 0){
+		return 1;
+	}
 }
