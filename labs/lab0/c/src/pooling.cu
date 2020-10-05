125fc7b79f6f1a3b6e8342239efdc86698d0b706
Cytosine <pengnan.fan@mail.mcgill.ca>
2020-10-05 17:01:44 +0800

    wrapup

--- labs/lab0/c/src/pooling.cu
+++ labs/lab0/c/src/pooling.cu
@@ -1,114 +1,140 @@
-#include <cuda_runtime.h>
-#include <device_launch_parameters.h>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <time.h>
+
 #include "lodepng.h"
 
-__global__ void compression(unsigned char* image, unsigned char* new_image, unsigned width, unsigned int size, unsigned int blocks_per_row) {
-	unsigned int index = threadIdx.x + (blockIdx.x % blocks_per_row) * blockDim.x;
-	unsigned int new_index = (threadIdx.x + blockIdx.x * blockDim.x) + 4;
 
-	if (index < size) {
-		for (int i = 0; i < 4; i++) {						// iterate through R, G, B, A
-			unsigned int max = image[index];
-			if (image[index + 4 + i] > max) {				// pixel to the right
-				max = image[index + 4 + i];
-			}
-			if (image[index + (4 * width) + i] > max) {		// pixel below
-				max = image[index + (4 * width) + i];
-			}
-			if (image[index + (4 * width) + 4 + i] > max) {	// pixel below & to the right
-				max = image[index + (4 * width) + 4 + i];
-			}
-			new_image[new_index + i] = max;
+
+__global__ void pool_kernel(unsigned char* image, unsigned char* new_image, unsigned width, unsigned height, int iter, int thread_define)
+{
+	//indexing
+	int index = blockIdx.x * blockDim.x + threadIdx.x;
+
+	//input number of threads
+	int num_thread = thread_define;
+
+	unsigned offset;
+	unsigned char top_left, top_right, bottom_left, bottom_right, max_value;
+
+	
+	if (index < thread_define) {
+		for (int k = 0; k < 4; k++) {
+			offset = iter * num_thread * 2 + index * 2;
+			offset += width * (offset / width);
+
+			//topleft, topright, bottomleft, bottomright
+			top_left = image[(offset) * 4 + k];
+			top_right = image[(offset + 1) * 4 + k];
+			bottom_left = image[(offset + width) * 4 + k];
+			bottom_right = image[(offset + width + 1) * 4 + k];
+			
+			max_value = 0;
+			
+			//find the max value 
+			if (top_left > max_value) max_value = top_left;
+			if (top_right > max_value) max_value = top_right;
+			if (bottom_left > max_value) max_value = bottom_left;
+			if (bottom_right > max_value) max_value = bottom_right;
+
+			new_image[(iter * num_thread + index) * 4 + k] = max_value;
 		}
 	}
 }
 
-void pooling(unsigned char *original, unsigned char *modified, int width, int height, int n_thread){
+void pooling(char* input_filename, char* output_filename, int thread_define)
+{
+	unsigned error;
+	unsigned char* image, * new_image;
+	unsigned width, height;
 
-	unsigned int size_image = width * height * 4 * sizeof(unsigned char);
 
-	unsigned char* cuda_image_pool, * cuda_new_image_pool;
-	cudaMalloc((void**)& cuda_image_pool, size_image);
-	cudaMalloc((void**)& cuda_new_image_pool, size_image);
+	//lode input file to image
+	error = lodepng_decode32_file(&image, &width, &height, input_filename); 
 
+	if (error) printf("error %u: %s\n", error, lodepng_error_text(error));
+
+	//malloc a new image
+	new_image = (unsigned char*)malloc(width * height * sizeof(unsigned char));  
+
+	//top_rightact start and end time 
+	clock_t start_time, end_time; 
+
+	start_time = clock();
 	
-	cudaMemcpy(cuda_image_pool, image, size_image, cudaMemcpyHostToDevice);
+	// parallel way of pooling
+	cudaSetDevice(0);
 
-	// maximum number of threads we can use is 1 per 16 pixel values
-	// that's because we can use maximum 1 thread per 2x2 area, and each pixel in that 2x2 area has 4 values
-	if (n_thread > ceil(size_image / 16)) {
-		n_thread = ceil(size_image / 16);
+	//move to GPU
+	unsigned char* image_cp;
+	cudaMallocManaged((void**)&image_cp, 4 * width * height * sizeof(unsigned char));
+	cudaMallocManaged((void**)&new_image, width * height * sizeof(unsigned char));
+
+
+	// copy image into image_cp
+	for (int i = 0; i < width * height * 4; i++) {
+		image_cp[i] = image[i];
 	}
 
-	
-	num_blocks = ceil((size_image / n_thread) / 16) + 1;
-	unsigned int blocks_per_row = ceil(width / n_thread);
+	//initialize new_image to 0 
+	for (int i = 0; i < width * height; i++) {
+		new_image[i] = 0;
+	}
 
-	// call method on GPU
-	compression <<< num_blocks, n_thread >>> (cuda_image_pool, cuda_new_image_pool, width, size_image, blocks_per_row);
+
+	//top_rightack how many iter we need 
+	int iter = 0;
+
+  //number of block 
+  int num_blocks = 1;
+  int num_threads = thread_define;
+  //int thread;
+ 
+ if (num_threads > 1024) {
+    while (num_threads > 1024) {
+        num_blocks *= 2; 
+        num_threads /= 2;
+        if (num_threads <= 1024) {
+              break; 
+              }
+    } 
+  }
+
+	while (iter < width * height / thread_define / 4) {
+		//maxmimum number of threads per block is 1024
+		pool_kernel << <(thread_define + 1023) / 1024, 1 >> > (image_cp, new_image, width, height, iter, thread_define);
+		iter++;
+	}
+		pool_kernel << <(thread_define + 1023) / 1024, (height * width) % 1024 >> > (image_cp, new_image, width, height, iter, thread_define);
+
 	cudaDeviceSynchronize();
 
-	// CPU copies input data from GPU back to CPU
-	unsigned char* new_image_pool = (unsigned char*)malloc(size_image);
-	cudaMemcpy(new_image_pool, cuda_new_image_pool, size_image, cudaMemcpyDeviceToHost);
-	cudaFree(cuda_image_pool);
-	cudaFree(cuda_new_image_pool);
+	end_time = clock();
 
-}
+	printf("time=%f\n", (double)(end_time - start_time) / (double)CLOCKS_PER_SEC);
 
-int check_if_png(char *name){
-	regex_t reg;
-	const char *pattern = "^.*\\.(png)?$";
-	regcomp(&reg, pattern, REG_EXTENDED);
-	const size_t nmatch = 1;
-	regmatch_t pmatch[1];
-	int status = regexec(&reg, name, nmatch, pmatch, 0);
-	regfree(&reg);
-	return (status == REG_NOMATCH) ? 0:1;
-}
+	////output the image
+	lodepng_encode32_file(output_filename, new_image, width / 2, height / 2); 
+	cudaFree(image); cudaFree(new_image); cudaFree(image_cp);
+	free(image);
 
-int main(int argc, char *argv[]){
-	printf("ECSE 420 Lab 0: Simple CUDA Processing - Pooling\n");
-
-	if (argc != 4) {
-		printf("RuntimeError: Please use the correct input format: ./ [original png].png [new png].png [n_thread]\n");
-		return 1;
-	}
+	//free(image_cp);
+	//free(new_image);
 	
-	if (check_if_png(argv[1]) && check_if_png(argv[2])) {
-		unsigned char *original, *modified;
-		unsigned error, width, height;
+}
 
-		error = lodepng_decode32_file(&original, &width, &height, argv[1]);
+int main(int argc, char* argv[])
+{
+	char* input_filename = argv[1];
+	char* output_filename_pooling = argv[2];
+  int thread_define = atoi(argv[3]);
+	//char* input_filename = "Test_1.png";
+	//char* output_filename_pooling = "Test_1_output.png";
+	
+	pooling(input_filename, output_filename_pooling, thread_define);
 
-		if (error) {
-			printf("ERROR %u: %s\n", error, lodepng_error_text(error));
-			free(original);
-			return 1;
-		} else {
-			modified = (unsigned char*)calloc(width*height*4, sizeof(unsigned char));
-
-			pooling(original, modified, width, height, atoi(argv[3]));
-
-			error = lodepng_encode32_file(argv[2], modified, width, height);
-
-			int status = 0;
-			if (error) {
-				printf("ERROR %u: %s\n", error, lodepng_error_text(error));
-				status = 1;
-			}
-			
-			free(original);
-			free(modified);
-			
-			printf("Done\n");
-			return status;
-		}
-	} else {
-		printf("RuntimeError: Invalid PNG inputs: %s %s\n", argv[1], argv[2]);
-		return 1;
-	}
+	return 0;
 }
