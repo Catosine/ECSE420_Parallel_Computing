e3ed0d37693a8a65be1c42bb400c0ba56ae83e5c
Cytosine <pengnan.fan@mail.mcgill.ca>
2020-10-16 16:14:39 +0800

    Create reader.h

---
+++ labs/lab1/c/inc/reader.h
@@ -0,0 +1,13 @@
+/* this flie takes three parameters:
+ *      char* file: path to the txt file formatted in csv
+ *      int** output: pointer stores data in the csv file
+ *      int* line: pointer to line count
+ */
+int read_csv(char* file, int** output, int *line);
+
+/* this flie takes three parameters:
+ *      char* file: path to the txt file formatted in csv
+ *      int* results: array of int results
+ *      int size: size of results array
+ */
+int save(char* file, int* results, int size);
