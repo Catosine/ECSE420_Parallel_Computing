b6cc8fb507f479deb7ae21c9f5474d750a2a0911
Cytosine <pengnan.fan@mail.mcgill.ca>
2020-10-19 23:15:13 +0800

    add prefetch

---
+++ labs/lab1/c/src/parallel_unified_prefetch.cu
@@ -0,0 +1,121 @@
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+#include <stdio.h>
+#include "reader.h"
+#include <stdlib.h>
+#include "gputimer.h"
+
+__global__ void kernel(int* data, int* result, int size)
+{
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	if (idx < size)
+	{
+		if (*(data+idx*3+2)==0)
+		{
+			//and
+			*(result+idx) = *(data+idx*3) && *(data+idx*3+1);
+		} 
+		else if (*(data+idx*3+2)==1)
+		{
+			//or
+			*(result+idx) = *(data+idx*3) || *(data+idx*3+1);
+		}
+		else if (*(data+idx*3+2)==2)
+		{
+			//nand
+			*(result+idx) = !(*(data+idx*3) && *(data+idx*3+1));
+		}
+		else if (*(data+idx*3+2)==3)
+		{
+			//nor
+			*(result+idx) = !(*(data+idx*3) || *(data+idx*3+1));
+		}
+		else if (*(data+idx*3+2)==4)
+		{
+			//xor
+			*(result+idx) = *(data+idx*3) ^ *(data+idx*3+1);
+		}
+		else if (*(data+idx*3+2)==5)
+		{
+			//xnor
+			*(result+idx) = !(*(data+idx*3) ^ *(data+idx*3+1));
+		}
+		else
+		{
+			//invalid
+			*(result+idx) = -1;
+		}
+		//*(result+idx)=idx;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	GpuTimer timer;
+	timer.Start();
+	printf("ECSE 420 Lab 1: Logic Gates Simulation - parallel_unified_prefetch\n");
+	if (argc != 4)
+	{
+		printf("RuntimeError: Wrong inputs.\n");
+		printf("You should input follows this format: ./parallel_unified <input_file_path> <input_file_length> <output_file_path>\n");
+		return 1;
+	}
+	
+	int size = atoi(argv[2]);
+
+	GpuTimer loadTimer;
+	loadTimer.Start();
+	int* cuda_file;
+	cudaMallocManaged((void **) &cuda_file, size*3*sizeof(int));
+
+	if(read_csv_array(argv[1], cuda_file, &size)==0){
+		
+		int deviceId = -1;
+		cudaGetDevice(&deviceId);
+		cudaMemPrefetchAsync(cuda_file, size*3*sizeof(int), deviceId);
+
+		int *cuda_output;
+		cudaMallocManaged((void **) &cuda_output, size*sizeof(int));
+		loadTimer.Stop();
+		float loadTime = loadTimer.Elapsed();
+
+		int block = size/1024;
+		if (size%1024) {
+			block++;
+		}
+		
+		GpuTimer parallelTimer;
+		parallelTimer.Start();
+		kernel <<<block, 1024>>> (cuda_file, cuda_output, size);
+		cudaDeviceSynchronize();
+		
+		parallelTimer.Stop();
+		float parallelTime = parallelTimer.Elapsed();
+		
+		cudaMemPrefetchAsync(cuda_output, size*sizeof(int), cudaCpuDeviceId);
+		
+		GpuTimer retriveTimer;
+		retriveTimer.Start();
+		save(argv[3], cuda_output, size);
+		retriveTimer.Stop();
+		float retriveTime = retriveTimer.Elapsed();
+
+		cudaFree(cuda_file);
+		cudaFree(cuda_output);
+		
+		timer.Stop();
+		float totalTime = timer.Elapsed();
+		
+		printf("Done\n");
+		printf("Load Time: %f ms\nParallel Time: %f ms\nRetrive Time: %f ms\n---------------\nTotal Time: %f ms\n", loadTime, parallelTime, retriveTime, totalTime);
+
+		return 0;
+	}
+	else
+	{
+		timer.Stop();
+		cudaFree(cuda_file);
+		return 1;
+	}
+	
+}
