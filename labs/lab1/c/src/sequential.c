9d7969b5ebbfcbafe688cf05b3004cf47ead6e7f
YuhangZhang <yuhang.zhang@mail.mcgill.ca>
2020-10-18 23:25:57 -0400

    sequential tests pass

--- labs/lab1/c/src/sequential.c
+++ labs/lab1/c/src/sequential.c
@@ -1,6 +1,9 @@
-#include<stdio.h>
-#include<math.h>
-#include<reader.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "../inc/reader.h"
+#include <unistd.h>
+#include <string.h>
 
 #define AND 0
 #define OR 1
@@ -8,6 +11,79 @@
 #define NOR 3
 #define XOR 4
 #define XNOR 5
+
+
+int csv_check(char* path)
+{
+    if (access(path, F_OK))
+    {   
+        printf("Cannot find the file at %s\n", path);
+        return -1;
+    } 
+    if (access(path, R_OK)) 
+    {
+        printf("Cannot read the file at %s\n", path);
+        return -1;
+    }
+    return 0;
+}
+
+int read_csv(char* file, int** output, int *line)
+{
+    if (csv_check(file))
+    {
+        //error detected
+        printf("RuntimeError: Cannot access the file at %s\n", file);
+        return -1;
+    } 
+    else 
+    {
+        // check passed
+        // allocate memory
+        *line = 0;
+
+        // read the csv file
+        FILE* f = fopen(file, "r");
+        char *tmp = (char *)calloc(10, sizeof(char));
+        char *token;
+        if (f)
+        {
+            while(fgets(tmp, 10, f))
+            {
+		        *(output+*line) = (int *)calloc(3, sizeof(int));
+                token = strtok(tmp, ",");
+                int i = 0;
+                while(token!=NULL)
+                {
+                    *(*(output+*line)+i) = atoi(token);
+                    token = strtok(NULL, ",");
+                    i++;
+                }
+		        *line = *line + 1;	
+            }
+        }
+        fclose(f);
+
+        tmp = NULL;
+        free(tmp);
+
+        return 0;
+    }
+}
+
+int save(char* file, int* results, int size)
+{
+    FILE *f = fopen(file, "w");
+    if (f)
+    {
+        for(int i=0; i<size; i++)
+        {
+            fprintf(f, "%d\n", *(results+i));
+        }
+    }
+    fclose(f);
+    return 0;
+}
 
 int logicGate(int input1, int input2, int operand){
     int output;
@@ -48,18 +124,17 @@ int logicGate(int input1, int input2, int operand){
             if(input1==1 && input2 ==0) output = 0;
             if(input1==1 && input2 ==1) output = 1;
             break;
-
-        return output;
     }
+    return output;
 }
 
 int main(int argc, char*argv[]){
-    if(argc != 3){
+    if(argc != 4){
         printf("Input should follow this specific pattern \n <input_file_path> <input_file_length> <output_file_path>.\n");
         exit(1);
     }
 
-    int num_lines = argv[2];
+    int num_lines = atoi(argv[2]);
     int **sourceInput = (int **)calloc(num_lines, sizeof(int *));
     int result[num_lines];
 
@@ -70,6 +145,7 @@ int main(int argc, char*argv[]){
     }
 
     free(sourceInput);
+
     switch(num_lines){
         case 10000:
             save("../../res/result_10000.txt", result, num_lines);
@@ -80,8 +156,8 @@ int main(int argc, char*argv[]){
         case 1000000:
             save("../../res/result_1000000.txt", result, num_lines);
             break;
-        printf("result file si saved\n");
+        printf("result file is saved\n");
     }
 
-    return 0;
+    EXIT_SUCCESS;
 }
