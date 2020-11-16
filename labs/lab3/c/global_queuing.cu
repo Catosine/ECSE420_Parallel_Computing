82c2d0239d0fee99137827a964201e574a4377e1
Cytosine <pengnan.fan@mail.mcgill.ca>
2020-11-16 20:01:19 +0800

    Update global_queuing.cu

--- labs/lab3/c/global_queuing.cu
+++ labs/lab3/c/global_queuing.cu
@@ -20,33 +20,33 @@ __global__ void kernel(int* nodePtrs, int* nodeNeightbors, int* nodeStatus, int*
         int nbr_idx = *(nodePtrs+node);
         int nbr_end = *(nodePtrs+node+1);
         for(; nbr_idx<=nbr_end; nbr_idx++)
-	{   
-                int nbr = *(nodeNeightbors+nbr_idx);
-                // check if visited
-                if(*(nodeStatus+nbr*4)==0)
-                {
-		    	atomicAdd(nodeStatus+nbr*4, 1);
-                    	int gate = *(nodeStatus+nbr*4+1);
-                    	int input1 = *(nodeStatus+nbr*4+2);
-                    	int input2 = *(nodeStatus+node*4+3);
-                    	int result = -3;
-                    	//AND
-                    	if (gate == 0) {result = (input1 & input2);}
-                    	//OR
-                    	else if (gate == 1) {result = (input1 | input2);}
-                    	//NAND
-                    	else if (gate == 2) {result = !(input1 & input2);}
-                    	//NOR
-                    	else if (gate == 3) {result = !(input1 | input2);}
-                    	//XOR
-                    	else if (gate == 4) {result = (input1 ^ input2);}
-                    	//XNOR
-                    	else if (gate == 5) {result = !(input1 ^ input2);}
-                    	else {result = -1;}
-                    	*(nodeStatus+nbr*4+3) = result;
-                    	int oidx = atomicAdd(idxOutputQueue, 1);
-                    	*(outputQueue+oidx) = nbr;
-        	}
+	    {   
+            int nbr = *(nodeNeightbors+nbr_idx);
+            // check if visited
+            if(*(nodeStatus+nbr*4)==0)
+            {
+                atomicAdd(nodeStatus+nbr*4, 1);
+                int gate = *(nodeStatus+nbr*4+1);
+                int input1 = *(nodeStatus+nbr*4+2);
+                int input2 = *(nodeStatus+node*4+3);
+                int result = -3;
+                //AND
+                if (gate == 0) {result = (input1 & input2);}
+                //OR
+                else if (gate == 1) {result = (input1 | input2);}
+                //NAND
+                else if (gate == 2) {result = !(input1 & input2);}
+                //NOR
+                else if (gate == 3) {result = !(input1 | input2);}
+                //XOR
+                else if (gate == 4) {result = (input1 ^ input2);}
+                //XNOR
+                else if (gate == 5) {result = !(input1 ^ input2);}
+                else {result = -1;}
+                *(nodeStatus+nbr*4+3) = result;
+                int oidx = atomicAdd(idxOutputQueue, 1);
+                *(outputQueue+oidx) = nbr;
+            }
         }
         // increment idx by 1
         idx = atomicAdd(idxCurrLevelNodes, 1);
@@ -109,6 +109,104 @@ int readFile3(char* name, int* data)
     return 0;
 }
 
+void compareFiles(char *file_name1, char *file_name2)
+{
+//get from https://www.tutorialspoint.com/c-program-to-compare-two-files-and-report-mismatches
+    FILE* fp1 = fopen(file_name1, "r");
+    FILE* fp2 = fopen(file_name2, "r");
+    // fetching character of two file
+    // in two variable ch1 and ch2
+    char ch1 = getc(fp1);
+    char ch2 = getc(fp2);
+
+    // error keeps track of number of errors
+    // pos keeps track of position of errors
+    // line keeps track of error line
+    int error = 0, pos = 0, line = 1;
+
+    // iterate loop till end of file
+    while (ch1 != EOF && ch2 != EOF)
+    {
+        pos++;
+
+        // if both variable encounters new
+        // line then line variable is incremented
+        // and pos variable is set to 0
+        if (ch1 == '\n' && ch2 == '\n')
+        {
+            line++;
+            pos = 0;
+        }
+
+        // if fetched data is not equal then
+        // error is incremented
+        if (ch1 != ch2)
+        {
+            error++;
+            printf("Line Number : %d \tError"
+                   " Position : %d \n", line, pos);
+        }
+
+        // fetching character until end of file
+        ch1 = getc(fp1);
+        ch2 = getc(fp2);
+    }
+
+    printf("Total Errors : %d\t", error);
+}
+
+void compareNextLevelNodeFiles(char *file_name1, char *file_name2)
+{
+
+    FILE* fp_1 = fopen(file_name1, "r");
+    if (fp_1 == NULL){
+        fprintf(stderr, "Couldn't open file for reading\n");
+        exit(1);
+    }
+
+    FILE* fp_2 = fopen(file_name2, "r");
+    if (fp_2 == NULL){
+        fprintf(stderr, "Couldn't open file for reading\n");
+        exit(1);
+    }
+
+    int counter = 0;
+    int len_1;
+    int len_2;
+    int length_file_1 = fscanf(fp_1, "%d", &len_1);
+    int length_file_2 = fscanf(fp_2, "%d", &len_2);
+
+    if(length_file_1 != length_file_2){
+        fprintf(stderr, "Wrong file length\n");
+        exit(1);
+    }
+    int *input1 = (int *)malloc(len_1 * sizeof(int));
+    int *input2 = (int *)malloc(len_2 * sizeof(int));
+
+    int temp1;
+    int temp2;
+
+    while ((fscanf(fp_1, "%d", &temp1) == 1) && (fscanf(fp_2, "%d", &temp2) == 1)) {
+        (input1)[counter] = temp1;
+        (input2)[counter] = temp2;
+        counter++;
+    }
+
+    sort(input1, len_1);
+    sort(input2, len_2);
+
+    for(int i=0; i< len_1; i++){
+        if(input1[i] != input2[i]){
+            fprintf(stderr, "Something goes wrong\n");
+            exit(1);
+        }
+    }
+
+    fprintf(stderr, "No errors!\n");
+    exit(1);
+
+}
+
 int main(int argc, char* argv[])
 {
     if (argc == 7)
@@ -148,7 +246,7 @@ int main(int argc, char* argv[])
 
         // run
         kernel <<<N_BLOCK, N_THREAD>>> (c_data1, c_data2, c_data3, c_data4, c_idxCurrLevelNodes, c_outputQueue, c_idxOutputQueue);
-	cudaDeviceSynchronize();
+	    cudaDeviceSynchronize();
 
         // data3 retrival
         cudaMemcpy(data3, c_data3, INPUT3_LEN*4*sizeof(int), cudaMemcpyDeviceToHost);
@@ -175,6 +273,9 @@ int main(int argc, char* argv[])
             fprintf(queueStatus, "%d\n", *(output+i));
         }
         fclose(queueStatus);
+
+        compareFiles(argv[5], "sol_nodeOutput.raw");
+        compareNextLevelNodeFiles(argv[6], "sol_nextLevelNodes.raw");
 
         // clean up
         cudaFree(c_data1);
