67391f2153bd05f5f11bdaad752355c2cea0dc9f
YuhangZhang <yuhang.zhang@mail.mcgill.ca>
2020-11-16 12:16:18 -0500

    finalization

--- labs/lab3/c/block_queuing.cu
+++ labs/lab3/c/block_queuing.cu
@@ -1,382 +1,237 @@
-#include <cuda_runtime.h>
-#include <device_launch_parameters.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include "gputimer.h"
-#define INPUT1_LEN 200001
-#define INPUT2_LEN 50000
-#define INPUT3_LEN 200000
-#define INPUT4_LEN 10000
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
 
-__global__ void kernel(int* nodePtrs, int* nodeNeightbors, int* nodeStatus, int* currLevelNodes, int* idxCurrLevelNodes, int* outputQueue, int* numOutputQueue, int sharedQueueSize)
+int read_input_one_two_four(int **input1, const char *filepath)
 {
-    extern __shared__ int blockQueue[];
-    __shared__ int blockCounter[1];
-    __shared__ int outputBlockOffset[1];
-
-    // initialize the counter
-    if (threadIdx.x==0) 
+    FILE *fp = fopen(filepath, "r");
+    if (fp == NULL)
     {
-        *blockCounter = 0;
-        *outputBlockOffset = 0;
-    }
-
-    __syncthreads();
-
-    int idx = atomicAdd(idxCurrLevelNodes, 1);
-     
-    while(idx < INPUT4_LEN)
-    {
-        // get node
-        int node = *(currLevelNodes+idx);
-        int nbr_idx = *(nodePtrs+node);
-        int nbr_end = *(nodePtrs+node+1);
-        for(; nbr_idx<=nbr_end; nbr_idx++)
-	    {   
-            int nbr = *(nodeNeightbors+nbr_idx);
-            if(*(nodeStatus+nbr*4)==0)
-	    {    
-		    atomicAdd(nodeStatus+nbr*4, 1);
-		    int gate = *(nodeStatus+nbr*4+1);
-		    int input1 = *(nodeStatus+nbr*4+2);
-		    int input2 = *(nodeStatus+node*4+3);
-		    int result = -3;
-		    //AND
-		    if (gate == 0) {result = (input1 & input2);}
-		    //OR
-		    else if (gate == 1) {result = (input1 | input2);}
-		    //NAND
-		    else if (gate == 2) {result = !(input1 & input2);}
-		    //NOR
-		    else if (gate == 3) {result = !(input1 | input2);}
-		    //XOR
-		    else if (gate == 4) {result = (input1 ^ input2);}
-		    //XNOR
-		    else if (gate == 5) {result = !(input1 ^ input2);}
-		    else {result = -1;}
-		    *(nodeStatus+nbr*4+3) = result;
-		    
-		    int bidx = atomicAdd(blockCounter, 1);
-		    atomicAdd(numOutputQueue, 1);
-		    if (bidx < sharedQueueSize){
-		    	*(blockQueue+bidx) = nbr;
-		    } else {
-			*(outputQueue+blockDim.x*sharedQueueSize+bidx-sharedQueueSize) = nbr;
-		    }
-		    /*
-		    int bidx = atomicAdd(blockCounter, 1);
-		    if (bidx >= sharedQueueSize)
-		    {
-			//copy to queue;
-			if (threadIdx.x==0)
-			{
-			    int boffset = atomicAdd(outputBlockOffset, 1);
-			    memcpy(outputQueue+boffset*sharedQueueSize, blockQueue, sharedQueueSize*sizeof(int));
-			    *blockCounter = 0;
-			}
-			__syncthreads();
-		    }
-		    //save to block mem
-		    bidx = atomicAdd(blockCounter, 1);
-		    *(blockQueue+bidx) = nbr;
-		    */
-	    }
-        }
-        // increment idx by 1
-        idx = atomicAdd(idxCurrLevelNodes, 1);
-    }
-    if (threadIdx.x==0)
-    {
-	    memcpy(outputQueue+blockIdx.x*sharedQueueSize, blockQueue, sharedQueueSize*sizeof(int));
-    }
-    __syncthreads();
-    /*
-    if (threadIdx.x==0)
-    {
-        int boffset = atomicAdd(outputBlockOffset, 0) - 1;
-        memcpy(outputQueue+boffset*sharedQueueSize, blockQueue, atomicAdd(blockCounter, 1)*sizeof(int));            
-    }
-    __syncthreads();
-    */
-}
-
-int readFile124(char* name, int* data)
-{
-    FILE* f = fopen(name, "r");
-    int offset = 0;
-    char* line = (char* )calloc(10, sizeof(char));
-    if (f)
-    {   
-        // omit the first
-        fgets(line, 10, f);
-        while(fgets(line, 10, f))
-        {
-            *(data+offset) = atoi(line);
-            offset++;
-        }
-    }
-    free(line);
-    line = NULL;
-    fclose(f);
-    return 0;
-}
-
-int readFile3(char* name, int* data)
-{
-    FILE* f = fopen(name, "r");
-    int offset = 0;
-    char* line = (char *)calloc(10, sizeof(char));
-    if (f) 
-    {
-        // omit the first
-        fgets(line, 10, f);
-        while(fgets(line, 10, f))
-        {
-            *(data+offset+0) = *(line+0) - '0';
-            *(data+offset+1) = *(line+2) - '0';
-            *(data+offset+2) = *(line+4) - '0';
-            if (*(line+6) == '-') 
-            {
-                *(data+offset+3) = -1;
-            }
-            else if (*(line+6) == '0') 
-            {
-                *(data+offset+3) = 0;
-            }
-            else if (*(line+6) == '1') 
-            {
-                *(data+offset+3) = 1;
-            }
-            offset+=4;
-        }
-    }
-    free(line);
-    line = NULL;
-    fclose(f);
-    return 0;
-}
-
-void sort(int *pointer, int size){
-    //get from https://stackoverflow.com/questions/13012594/sorting-with-pointers-instead-of-indexes
-    int *i, *j, temp;
-    for(i = pointer; i < pointer + size; i++){
-	    printf("count %d\n", i-pointer);
-	for(j = i + 1; j < pointer + size; j++){
-            if(*j < *i){
-                temp = *j;
-                *j = *i;
-                *i = temp;
-            }
-        }
-    }
-}
-
-void compareFiles(char *file_name1, char *file_name2)
-{
-//get from https://www.tutorialspoint.com/c-program-to-compare-two-files-and-report-mismatches
-    FILE* fp1 = fopen(file_name1, "r");
-    FILE* fp2 = fopen(file_name2, "r");
-    // fetching character of two file
-    // in two variable ch1 and ch2
-    char ch1 = getc(fp1);
-    char ch2 = getc(fp2);
-
-    // error keeps track of number of errors
-    // pos keeps track of position of errors
-    // line keeps track of error line
-    int error = 0, pos = 0, line = 1;
-
-    // iterate loop till end of file
-    while (feof(fp1) && feof(fp2))
-    //while (ch1 != EOF && ch2 != EOF)
-    {
-        pos++;
-
-        // if both variable encounters new
-        // line then line variable is incremented
-        // and pos variable is set to 0
-        if (ch1 == '\n' && ch2 == '\n')
-        {
-            line++;
-            pos = 0;
-        }
-
-        // if fetched data is not equal then
-        // error is incremented
-        if (ch1 != ch2)
-        {
-            error++;
-            printf("Line Number : %d \tError"
-                   " Position : %d \n", line, pos);
-        }
-
-        // fetching character until end of file
-        ch1 = getc(fp1);
-        ch2 = getc(fp2);
-    }
-
-    printf("Total Errors : %d\n", error);
-}
-
-void compareNextLevelNodeFiles(char *file_name1, char *file_name2)
-{
-
-    FILE* fp_1 = fopen(file_name1, "r");
-    if (fp_1 == NULL){
-        fprintf(stderr, "Couldn't open file for reading\n");
-        exit(1);
-    }
-
-    FILE* fp_2 = fopen(file_name2, "r");
-    if (fp_2 == NULL){
         fprintf(stderr, "Couldn't open file for reading\n");
         exit(1);
     }
 
     int counter = 0;
-    int len_1;
-    int len_2;
-    int length_file_1 = fscanf(fp_1, "%d", &len_1);
-    int length_file_2 = fscanf(fp_2, "%d", &len_2);
-
-    if(length_file_1 != length_file_2){
-        fprintf(stderr, "Wrong file length\n");
-        exit(1);
-    }
-    int *input1 = (int *)malloc(len_1 * sizeof(int));
-    int *input2 = (int *)malloc(len_2 * sizeof(int));
-
-    int temp1;
-    int temp2;
-
-    while ((fscanf(fp_1, "%d", &temp1) == 1) && (fscanf(fp_2, "%d", &temp2) == 1)) {
-        //printf("Counter = %d\n", counter);
-	(input1)[counter] = temp1;
-        (input2)[counter] = temp2;
-        counter++;
-    }
-
-    sort(input1, len_1);
-    sort(input2, len_2);
-
-    for(int i=0; i< len_1; i++){
-	    printf("#%d (%d %d)\n", i, input1[i], input2[i]);
-        if(input1[i] != input2[i]){
-            fprintf(stderr, "Something goes wrong\n");
-            exit(1);
+    int len;
+    if (fscanf(fp, "%d", &len) == 1)
+    {
+        cudaMallocManaged((int **)input1, len * sizeof(int));
+        int temp1;
+        while (fscanf(fp, "%d", &temp1) == 1)
+        {
+            (*input1)[counter] = temp1;
+            counter++;
         }
     }
 
-    printf("No errors!\n");
-
+    fclose(fp);
+    return len;
 }
 
-int main(int argc, char* argv[])
+int read_input_three(int **input1, int **input2, int **input3, int **input4, const char *filepath)
 {
-    if (argc == 10)
-    {   
-        // read parameters
-        int numBlock = atoi(argv[1]);
-        int blockSize = atoi(argv[2]);
-        int sharedQueueSize = atoi(argv[3]);
-
-        // read input 1
-        int* data1 = (int* )calloc(INPUT1_LEN, sizeof(int));
-        readFile124(argv[4], data1);
-        // read input 2
-        int* data2 = (int* )calloc(INPUT2_LEN, sizeof(int));
-        readFile124(argv[5], data2);
-        // read input 3
-        int* data3 = (int* )calloc(INPUT3_LEN*4, sizeof(int));
-        readFile3(argv[6], data3);
-        // read input 1
-        int* data4 = (int* )calloc(INPUT4_LEN, sizeof(int));
-        readFile124(argv[7], data4);
-        // setup idxNextLevelNodes
-        int idxCurrLevelNodes = 0;
-	int numOutputQueue = 0;
-
-        // cuda setup
-        int *c_data1, *c_data2, *c_data3, *c_data4, *c_outputQueue, *c_idxCurrLevelNodes, *c_numOutputQueue;
-        cudaMalloc((void **) &c_data1, INPUT1_LEN*sizeof(int));
-        cudaMalloc((void **) &c_data2, INPUT2_LEN*sizeof(int));
-        cudaMalloc((void **) &c_data3, INPUT3_LEN*4*sizeof(int));
-        cudaMalloc((void **) &c_data4, INPUT4_LEN*sizeof(int));
-        cudaMalloc((void **) &c_outputQueue, INPUT3_LEN*sizeof(int));
-        cudaMalloc((void **) &c_idxCurrLevelNodes, sizeof(int));
-	cudaMalloc((void **) &c_numOutputQueue, sizeof(int));
-        cudaMemcpy(c_data1, data1, INPUT1_LEN*sizeof(int), cudaMemcpyHostToDevice);
-        cudaMemcpy(c_data2, data2, INPUT2_LEN*sizeof(int), cudaMemcpyHostToDevice);
-        cudaMemcpy(c_data3, data3, INPUT3_LEN*4*sizeof(int), cudaMemcpyHostToDevice);
-        cudaMemcpy(c_data4, data4, INPUT4_LEN*sizeof(int), cudaMemcpyHostToDevice);
-        cudaMemcpy(c_idxCurrLevelNodes, &idxCurrLevelNodes, sizeof(int), cudaMemcpyHostToDevice);
-	cudaMemcpy(c_numOutputQueue, &numOutputQueue, sizeof(int), cudaMemcpyHostToDevice);
-        // run
-        GpuTimer timer;
-        timer.Start();
-        kernel <<<numBlock, blockSize, sharedQueueSize*sizeof(int)>>> (c_data1, c_data2, c_data3, c_data4, c_idxCurrLevelNodes, c_outputQueue, c_numOutputQueue, sharedQueueSize);
-	cudaDeviceSynchronize();
-        timer.Stop();
-        printf("Block Queuing Kernel Runtime (blockSize=%d, numBlock=%d, blockQueueCapacity=%d): %f ms\n", numBlock, blockSize, sharedQueueSize, timer.Elapsed());
-
-        // data3 retrival
-        cudaMemcpy(data3, c_data3, INPUT3_LEN*4*sizeof(int), cudaMemcpyDeviceToHost);
-
-        // outputs retrival
-	cudaMemcpy(&numOutputQueue, c_numOutputQueue, sizeof(int), cudaMemcpyDeviceToHost);
-	numOutputQueue--;
-        int* output = (int *)calloc(numOutputQueue, sizeof(int));
-        cudaMemcpy(output, c_outputQueue, numOutputQueue*sizeof(int), cudaMemcpyDeviceToHost);
-
-        // save gate status
-        FILE *gateStatus = fopen(argv[8], "w");
-        fprintf(gateStatus, "%d\n", INPUT3_LEN);
-        for(int i=0; i<INPUT3_LEN; i++)
-        {
-            fprintf(gateStatus, "%d\n", *(data3+i*4+3));
-        }
-        fclose(gateStatus);
-
-        // save output
-        FILE *queueStatus = fopen(argv[9], "w");
-        fprintf(queueStatus, "%d\n", numOutputQueue);
-        for(int i=0; i<numOutputQueue; i++)
-        {
-            fprintf(queueStatus, "%d\n", *(output+i));
-        }
-        fclose(queueStatus);
-	
-	printf("Comparing nodeOutput.raw...\n");
-        compareFiles(argv[8], "sol_nodeOutput.raw");
-	printf("Comparing nextLevelNodes.raw...\n");
-        compareNextLevelNodeFiles(argv[9], "sol_nextLevelNodes.raw");
-
-        // clean up
-        cudaFree(c_data1);
-        cudaFree(c_data2);
-        cudaFree(c_data3);
-        cudaFree(c_data4);
-        cudaFree(c_outputQueue);
-        cudaFree(c_idxCurrLevelNodes);
-	cudaFree(c_numOutputQueue);
-
-        free(data1);
-        free(data2);
-        free(data3);
-        free(data4);
-        free(output);
-
-        data1 = NULL;
-        data2 = NULL;
-        data3 = NULL;
-        data4 = NULL;
-        output = NULL;
-    
-    }
-    else
+    FILE *fp = fopen(filepath, "r");
+    if (fp == NULL)
     {
-        printf("RuntimeError: Please follow the correct input format as: /block_queuing <numBlock> <blockSize> <sharedQueueSize> <path_to_input_1.raw> <path_to_input_2.raw> <path_to_input_3.raw> <path_to_input_4.raw> <output_nodeOutput_filepath> <output_nextLevelNodes_filepath> ");
+        fprintf(stderr, "Couldn't open file for reading\n");
+        exit(1);
+    }
+
+    int counter = 0;
+    int len;
+    if (fscanf(fp, "%d", &len) == 1)
+    {
+        cudaMallocManaged((int **)input1, len * sizeof(int));
+        cudaMallocManaged((int **)input2, len * sizeof(int));
+        cudaMallocManaged((int **)input3, len * sizeof(int));
+        cudaMallocManaged((int **)input4, len * sizeof(int));
+        int temp1;
+        int temp2;
+        int temp3;
+        int temp4;
+        while (fscanf(fp, "%d,%d,%d,%d", &temp1, &temp2, &temp3, &temp4) == 4)
+        {
+            (*input1)[counter] = temp1;
+            (*input2)[counter] = temp2;
+            (*input3)[counter] = temp3;
+            (*input4)[counter] = temp4;
+            counter++;
+        }
+    }
+
+    fclose(fp);
+    return len;
+}
+
+void write_output(int nNodes, int nNextLevelNodes,
+                  const int *nodesOutput, const int *nextLevelNodes,
+                  const char *output_file_1, const char *output_file_2)
+{
+    FILE *fp = fopen(output_file_1, "w");
+    if (fp == NULL)
+    {
+        fprintf(stderr, "Couldn't open file for writing\n");
+        exit(1);
+    }
+    fprintf(fp, "%d\n", nNodes);
+    for (int i = 0; i < nNodes; ++i)
+    {
+        fprintf(fp, "%d\n", nodesOutput[i]);
+    }
+    fclose(fp);
+
+    fp = fopen(output_file_2, "w");
+    if (fp == NULL)
+    {
+        fprintf(stderr, "Couldn't open file for writing\n");
+        exit(1);
+    }
+    fprintf(fp, "%d\n", nNextLevelNodes);
+    for (int i = 0; i < nNextLevelNodes; ++i)
+    {
+        fprintf(fp, "%d\n", nextLevelNodes[i]);
+    }
+    fclose(fp);
+}
+
+__device__ int calcGate(int gate, int inp1, int inp2)
+{
+    switch (gate)
+    {
+    case 0:
+        return inp1 & inp2;
+    case 1:
+        return inp1 | inp2;
+    case 2:
+        return !(inp1 & inp2);
+    case 3:
+        return !(inp1 | inp2);
+    case 4:
+        return inp1 ^ inp2;
+    case 5:
+        return !(inp1 ^ inp2);
+    default:
+        return 0;
+    }
+}
+
+__global__ void BFS(int nNodePtrs, int nNodeNeighours, int nNodes, int nCurrLevelNodes, int blockQueCap,
+                    const int *nodePtrs, const int *nodeNeighbours, int *nodesVisited, const int *nodesGate,
+                    const int *nodesInput, int *nodesOutput, const int *currLevelNodes,
+                    int *nextLevelNodes, int *nNextLevelNodes)
+{
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+    int tid = threadIdx.x;
+
+    if (index >= nCurrLevelNodes)
+    {
+        return;
+    }
+
+    __shared__ int counter[1];
+    __shared__ int localNextLevelNodes[64];
+
+    if (tid == 0)
+    {
+        *counter = 0;
+    }
+
+    __syncthreads();
+
+    for (int i = index; i < nCurrLevelNodes; i += stride)
+    {
+        int node = currLevelNodes[i];
+        for (int nbrIdx = nodePtrs[node]; nbrIdx < nodePtrs[node + 1]; nbrIdx++)
+        {
+            int neighbour = nodeNeighbours[nbrIdx];
+            if (atomicCAS(&nodesVisited[neighbour], 0, 1) == 0)
+            {
+                nodesOutput[neighbour] = calcGate(nodesGate[neighbour], nodesOutput[node], nodesInput[neighbour]);
+
+                int _counter = atomicAdd(counter, 1);
+                if (_counter > blockQueCap - 1)
+                {
+                    nextLevelNodes[atomicAdd(nNextLevelNodes, 1)] = neighbour;
+                }
+                else
+                {
+                    localNextLevelNodes[_counter] = neighbour;
+                }
+            }
+        }
+    }
+
+    __syncthreads();
+
+    if (tid == 0)
+    {
+        memcpy(nextLevelNodes + atomicAdd(nNextLevelNodes, blockQueCap),
+               localNextLevelNodes, blockQueCap * sizeof(int));
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 10)
+    {
+        fprintf(stderr, "%s <nBlocks> <blockSz> <blockQueCap> <input1> <input2> <input3> <input4> <output1> <output2>\n", argv[0]);
         return 1;
     }
+
+    const int nBlocks = atoi(argv[1]);
+    const int blockSz = atoi(argv[2]);
+    const int blockQueCap = atoi(argv[3]);
+
+    const char *input_file_1 = argv[4];
+    const char *input_file_2 = argv[5];
+    const char *input_file_3 = argv[6];
+    const char *input_file_4 = argv[7];
+
+    const char *output_file_1 = argv[8];
+    const char *output_file_2 = argv[9];
+
+    int *nodePtrs;
+    int nNodePtrs = read_input_one_two_four(&nodePtrs, input_file_1);
+    printf("nNodePtrs : %d\n", nNodePtrs);
+
+    int *nodeNeighbours;
+    int nNodeNeighbours = read_input_one_two_four(&nodeNeighbours, input_file_2);
+    printf("nNodeNeighbours : %d\n", nNodeNeighbours);
+
+    int *nodesVisited;
+    int *nodesGate;
+    int *nodesInput;
+    int *nodesOutput;
+    int nNodes = read_input_three(&nodesVisited, &nodesGate, &nodesInput, &nodesOutput, input_file_3);
+    printf("nNodes : %d\n", nNodes);
+
+    int *currLevelNodes;
+    int nCurrLevelNodes = read_input_one_two_four(&currLevelNodes, input_file_4);
+    printf("nCurrLevelNodes : %d\n", nCurrLevelNodes);
+
+    int *nextLevelNodes;
+    int *nNextLevelNodes;
+    cudaMallocManaged((int **)&nextLevelNodes, nNodePtrs * sizeof(int));
+    cudaMallocManaged((int **)&nNextLevelNodes, sizeof(int));
+    *nNextLevelNodes = 0;
+    BFS<<<nBlocks, blockSz>>>(nNodePtrs, nNodeNeighbours, nNodes, nCurrLevelNodes, blockQueCap,
+                              nodePtrs, nodeNeighbours, nodesVisited, nodesGate,
+                              nodesInput, nodesOutput, currLevelNodes,
+                              nextLevelNodes, nNextLevelNodes);
+    cudaDeviceSynchronize();
+    printf("nNextLevelNodes : %d\n", *nNextLevelNodes);
+
+    write_output(nNodes, *nNextLevelNodes, nodesOutput, nextLevelNodes, output_file_1, output_file_2);
+
+    cudaFree(nodePtrs);
+    cudaFree(nodeNeighbours);
+    cudaFree(nodesVisited);
+    cudaFree(nodesGate);
+    cudaFree(nodesInput);
+    cudaFree(nodesOutput);
+    cudaFree(currLevelNodes);
+    cudaFree(nextLevelNodes);
+    cudaFree(nNextLevelNodes);
 }
